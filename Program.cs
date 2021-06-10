02cd5ef81e35be3a5162f2b3710b546fb81e2674
SirMartin <sirmartin@gmail.com>
2021-06-10 17:59:56 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using iText.Kernel.Pdf;
+using System.IO;
+
+namespace PdfPasswordRemover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change the values to the right ones.
+
+            // The path where the original PDFs are located.
+            string mainPath = @"c:\pathToPDFsWithPassword";
+            // The path where do you want to save the decrypted PDFs. I use a subfolder in the same path.
+            string decryptedFilesPath = Path.Combine(mainPath, "NowDecrypted");
+            // The password of the PDFs.
+            var password = "MySuperSecretPassword";
+
+            // Start the process.
+            var files = Directory.GetFiles(mainPath, "*.pdf");
+            var pdfManipulator = new PdfManipulation();
+
+            foreach (var file in files)
+            {
+                var dest = Path.Combine(decryptedFilesPath, Path.GetFileName(file));
+                pdfManipulator.ManipulatePdf(file, dest, password);
+            }
+        }
+
+        public class PdfManipulation
+        {
+            public void ManipulatePdf(string src, string dest, string password)
+            {
+                var passwordBytes = new System.Text.ASCIIEncoding().GetBytes(password);
+
+                var reader = new MyReader(src, passwordBytes);
+                reader.SetUnethicalReading(true);
+                reader.DecryptOnPurpose();
+
+                if (!Directory.Exists(Path.GetDirectoryName(dest)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(dest));
+                }
+
+                var pdfDoc = new PdfDocument(reader, new PdfWriter(dest));
+                pdfDoc.Close();
+                reader.Close();
+            }
+        }
+
+        class MyReader : PdfReader
+        {
+            public MyReader(string filename, byte[] passwordBytes) : base(filename, new ReaderProperties().SetPassword(passwordBytes)) { }
+            public void DecryptOnPurpose()
+            {
+                encrypted = false;
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,12 @@
+﻿# PDF Decryptor
+
+## What is this?
+
+The tool is only effective if all the PDFs have the same password.
+
+I had around 50 pdfs that I have to open with a password, I want to store them, but I have the risk to forget the password to open them, so I just want to remove the password. I can do that in the classic
+way and just open the PDF and saving without the password, but that means I need to make the same action a lot of times, and I don't like it, I preferred the option of making this small tool.
+
+## Usage
+
+Change the variables in the Program.cs file, and run it.
